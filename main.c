c5ee9dbfbb1a686aad569c4321ae9bfe9af5fb08
Vinicius Vendramini <vinivendra@gmail.com>
2013-05-31 10:07:25 -0300

    Começo da implementação do 'e'. O processamento do texto ainda não está 100%.

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 
 #define YES 1
 #define NO 0
+typedef int BOOL;
+
 #define STRING_MAX 50
 
 void stringCopy(char *destiny, char *source);
@@ -26,20 +28,25 @@ int main (int argc, char *argv[]) {
     fpos_t *aux = malloc(sizeof(fpos_t));
     int tokenNumber = 0;
     
+    BOOL e, a, v, V, t, d, l, L, s;
+    
     int i = 0;
     int j = 0;
     
     /* Inicializações */
     
-    initItem();
+    initItem();             /* Inicializa Item e as árvores, para podermos começar a */
+    STinit();               /* criar instâncias deles. */
     
-    STinit();
     words = initTree();
     lemmas = initTree();
     
+    e = a = v = V = t = d = l = L = s = NO; /* Seta todas as flags com NO,
+                                                para evitar erros de inicialização */
+    
     /* Leitura de parâmetros */
     if (argc < 2) {
-        fprintf(stderr, "Por favor, passe o nome do arquivo de entrada como parâmetro.\n");
+        fprintf(stderr, "Usage: %s -f<Nome do arquivo de entrada>\n", argv[0]);
         exit(-1);
     }
     fileName = argv[1]+2;
@@ -52,9 +59,12 @@ int main (int argc, char *argv[]) {
         exit(-1);
     }
     
+    
+    /* Preparação para processar o arquivo */
     fgetpos(file, sentenceLocation);
     fgetc(file);
-
+    
+    
     /* Processamento do arquivo */
     while (!feof(file)) {
         
@@ -62,9 +72,9 @@ int main (int argc, char *argv[]) {
         fscanf(file, "entence #%*d (%d tokens):\n", &tokenNumber);
         
         /* Pula a frase. Lê caracteres até achar um '['. Se for o começo de um token,
-            volta até o caracter imediatamente anterior e sai do loop para começar a
-            ler os tokens. Caso contrário, volta o suficiente para continuar a leitura
-            caracter por caracter logo após o '['. */
+         volta até o caracter imediatamente anterior e sai do loop para começar a
+         ler os tokens. Caso contrário, volta o suficiente para continuar a leitura
+         caracter por caracter logo após o '['. */
         while (1) {
             fgetpos(file, aux);
             *c = fgetc(file);
@@ -88,14 +98,14 @@ int main (int argc, char *argv[]) {
             Item *newLemma = malloc(sizeof(Item));
             Item *conflict = NULL;
             
-                                                        /* Inicializa os novos itens */
+            /* Inicializa os novos itens */
             newWord->literal = malloc(STRING_MAX*sizeof(char));
             newWord->lema = newLemma;
             newWord->prox = NULL;
             newWord->list = malloc(sizeof(sentence));
             newWord->list->position = *sentenceLocation;
             newWord->list->prox = NULL;
-            
+                        
             newLemma->literal = malloc(STRING_MAX*sizeof(char));
             newLemma->lema = NULL;
             newLemma->prox = newWord;
@@ -103,7 +113,7 @@ int main (int argc, char *argv[]) {
             
             
             fscanf(file, "%s", c);                      /* Lê "[Text=word" */
-
+            
             strcpy(newWord->literal, (c+6));            /* Coloca a palavra encontrada no novo item */
             
             while(1) {                                  /* Percorre até o achar o lema */
@@ -111,7 +121,7 @@ int main (int argc, char *argv[]) {
                 if (strncmp(c, "Lemma=", 6)==0)
                     break;
             }
-                        
+            
             for (j = 0;; j++) {                         /* O lema vai terminar com ']'; queremos que termine com '\0' */
                 if (*(c+6+j) == ']') {
                     c[6+j] = '\0';
@@ -122,10 +132,10 @@ int main (int argc, char *argv[]) {
             strcpy(newLemma->literal, (c+6));           /* Coloca o lema no novo item */
             
             
-            words = STinsert(words, newWord, conflict);     /* Tentativa de inserir o item word na árvore T1 
-                                                                Conflict é um ponteiro que vai apontar para o item
-                                                                que já estava na árvore, caso tentemos adicionar
-                                                                uma palavra repetida.*/
+            words = STinsert(words, newWord, conflict);     /* Tentativa de inserir o item word na árvore T1
+                                                             Conflict é um ponteiro que vai apontar para o item
+                                                             que já estava na árvore, caso tentemos adicionar
+                                                             uma palavra repetida.*/
             
             if (conflict == NULL) {                         /* A palavra ainda não existia */
                 lemmas = STinsert(lemmas, newLemma, conflict);
@@ -148,35 +158,114 @@ int main (int argc, char *argv[]) {
         
         fgetc(file);                        /* "Consumimos" o espaço, o enter e o 'S' logo depois dos tokens. */
         fgetc(file);                        /* Assim, se estivermos no final, o indicador de EOF será ligado */
-        fgetpos(file, sentenceLocation);    /* E sabemos que podemos sair do loop. */
-        fgetc(file);                        /* Ainda assim, é importante que a sentenceLocation esteja no lugar certo, 
-                                                ou seja, um caracter antes da "Sentence #%d: (%d tokens)"*/
+        fgetpos(file, sentenceLocation);    /* e sabemos que podemos sair do loop com o "feof(file)". */
+        fgetc(file);                        /* Ainda assim, é importante que a sentenceLocation esteja no lugar certo,
+                                             ou seja, um caracter antes da "Sentence #%d: (%d tokens)"*/
     }               /* while(!foef(file)) */
     
     /* Todo o arquivo já foi processado nesse ponto; os tokens já foram lidos, e os itens já foram inseridos
-        nas árvores. Agora, lemos da entrada padrão os comandos que o usuário passar. */
+     nas árvores. Agora, lemos da entrada padrão os comandos que o usuário passar. */
     
-    
-    
-    STprint(words);
-    STprint(lemmas);
+#warning Adicionar informações sobre os comandos na usage, tanto lá em cima quando logo aqui em baixo
     
     /* Leitura de comandos a partir da stdin */
-    while (1) {
-        scanf("%s", c);
+    
+    printf ("Por favor, digite um comando: ");
+    
+    while (scanf("%s", c) == 1) {   /* Aceitamos que o usuário saia do programa também ao
+                                        pressionar ctrl+D, que é o modo default de encerrar
+                                        input da stdin */
+        char *p;                /* p vai "percorrer" a string c */
         
-        if (c[1] == 'F') {
-            printf("Saindo do programa.\n");
-            return 0;
+        if (c == '\0') {        /* Se o usuário só deu enter, mostramos o modo de utilização do programa. */
+            printf ("É assim que funciona: HUE.\n");
+        }
+        
+        p = c + 1;              /* Ignora o '-', como em "-aV" */
+        
+        while (*p != '\0') {
+            if (*p == 'F') {        /* -F quer dizer que o usuário deseja sair do programa */
+                printf("Saindo do programa.\n");
+                return 0;
+            }
+            else if (*p == 'e') {   /* Qualquer outra letra acionará uma flag, */
+                e = YES;            /* para sabermos o que fazer em cada caso. */
+                scanf("%s", c);     /* c será a palavra para procurar */
+            }
+            else if (*p == 'a')
+                a = YES;
+            else if (*p == 'v')
+                v = YES;
+            else if (*p == 'V')
+                V = YES;
+            else if (*p == 't')
+                t = YES;
+            else if (*p == 'd')
+                d = YES;
+            else if (*p == 'l')
+                l = YES;
+            else if (*p == 'L')
+                L = YES;
+            else if (*p == 's')
+                s = YES;
+            
+            p++;
         }
         
         
-    }
+        if (e == YES) {
+            
+            Item *word = STsearch(c, words);
+            sentence *s = NULL;
+            
+            if (word == getNULLitem()) {
+                printf("A palavra procurada não está presente no texto.\n\n");
+                printf ("Por favor, digite um comando: ");
+                continue;
+            }
+            
+            
+            
+            for (s = word->list; s != NULL; s = s->prox) {
+                fpos_t *aux = malloc(sizeof(fpos_t));
+                
+                fsetpos(file, &(s->position));
+                fscanf (file, "Sentence #%*d (%*d tokens):");
+                printf ("\n");
+                
+                while (1) {
+                    fgetpos(file, aux);
+                    *c = fgetc(file);
+                    if (*c == '[') {
+                        fscanf(file, "%s", c);
+                        if (strncmp(c, "Text=", 5)==0) {
+                            fsetpos(file, aux);
+                            break;
+                        }
+                        else {
+                            fsetpos(file, aux);
+                            fgetc(file);
+                            printf("%c", *c);
+                        }
+                    }
+                    else printf("%c", *c);
+                }       /* while (1), imprime a sentence */
+                
+                printf("\n");
+            }           /* for (s = word->list; s != NULL; s = s->prox), percorre as sentences */
+        }               /* if (e == YES) */
+        
+        
+        
+        printf ("Por favor, digite um comando: ");
+        e = a = v = V = t = d = l = L = s = NO;
+        
+    }           /* while (scanf("%s", c) == 1) */
     
     
     
     
-
+    
     
     /* Finalizando */
     fclose(file);
